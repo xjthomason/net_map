e1e399bf2dbe5029f7dd93ae8c04c7c93a062c4b
The Joker <joshua.thomason@avx.com>
2018-03-11 22:42:18 -0400

    quick fixes

--- net_map.py
+++ net_map.py
@@ -7,29 +7,38 @@ def ip_scan(address): # ping each ip
 
     a = os.system
     try:
-        status = a('ping %s' % address)
-        ip_list.append(address)
+        status = a('ping -c 1 %s' % address)
     except Exception, e:
     	print e
     print status
 
-def ipnetwork(network): # interpret network or address
+def ipnetwork(network): # interpret network
 
-    ip = ipaddress.ip_network(network)
-    print ip
-    return
-
+    ip = ipaddress.ip_network(unicode(network))
+    for i in ip:
+        try:
+            ip_scan(i)
+            ip_list.append(i)
+        except Exception, e:
+            print e
+            
 def network_map(ip_list): # pump list of ips into function to map network
 
     return
 
 def main():
 
-    parser = argparse.ArgumentParser('usage%prog ' % '-h <Host Address> OR -n <IP range>')
-    parser.add_argument('-h', '--host', help='Please enter a host address')
+    parser = argparse.ArgumentParser('usage%prog ' '-h <Host Address> -n <IP range>')
+    parser.add_argument('-H', '--host', help='Please enter a host address')
     parser.add_argument('-n', '--network', help='Please enter a network range')
-    args = parse.args()
+    args = parser.parse_args()
     host = args.host
     network = args.network
 
-    
+    if host == None and network:
+        ipnetwork(network)
+
+    if network == None and host:
+        ip_scan(host)
+
+main()
