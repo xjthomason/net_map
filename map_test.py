08a54e3a1acc6c5927f2fc35d115320fad7de947
The Joker <joshua.thomason@avx.com>
2018-03-12 22:20:00 -0400

    trying

---
+++ map_test.py
@@ -0,0 +1,30 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def draw_graph(graph):
+
+    # extract nodes from graph
+    nodes = set([n1 for n1, n2 in graph] + [n2 for n1, n2 in graph])
+
+    # create networkx graph
+    G=nx.Graph()
+
+    # add nodes
+    for node in nodes:
+        G.add_node(node)
+
+    # add edges
+    for edge in graph:
+        G.add_edge(edge[0], edge[1])
+
+    # draw graph
+    pos = nx.shell_layout(G)
+    nx.draw(G, pos)
+
+    # show graph
+    plt.show()
+
+# draw example
+DG = 20
+graph = [(DG, 21),(DG, 22),(DG, 23), (DG, 24),(DG, 25), (DG, 26)]
+draw_graph(graph)
