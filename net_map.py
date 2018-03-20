0d380e16c213140836184a1731b4f68afb6a1a56
root <root@localhost.localdomain>
2018-03-20 15:54:59 -0400

    progress_update

--- net_map.py
+++ net_map.py
@@ -1,10 +1,12 @@
-import nmap, argparse, ipaddress
+import nmap, argparse, ipaddress, progress_bar, os
 import networkx as nx
-from multiprocessing import Process # TODO see how subprocess can assist with this
+from multiprocessing import Process
 from prettytable import PrettyTable
+from time import sleep # for Progress Bar
 
 ip_list = [] # list of all discovered IPs
 ips_hostnames = PrettyTable(['IP','hostnames','OS']) # list of all discovered reverse DNS names
+a = os.system # clear screen after every progress update
 
 def ipnetwork(network): # interpret network
 
@@ -20,27 +22,32 @@ def ipnetwork(network): # interpret network
 		pass
         except Exception, e:
             continue
+        
     
-    print "%d # of Hosts are up." % len(ip_list)
-
+    print "%d Hosts are up." % len(ip_list)
+    
+    progress_bar.printProgressBar(0, len(ip_list), prefix='Scanning %d of %d...\nProgress:' % (1,len(ip_list)), suffix='Complete', length=50) # initialize progress bar
     iphostname(ip_list)
 
 def iphostname(ip_list): # pull hostnames for list of IPs
-
-    for ip in ip_list:
-        try:
-            nmScan = nmap.PortScanner()
-            results = nmScan.scan(str(ip),arguments='-A -T5')
-            hostname = results['scan'][str(ip)]['hostnames'][0]['name']
-            OS = results['scan'][str(ip)]['osmatch'][0]['name']
-            if OS == None:
-                ips_hostnames.add_row([ip, hostname, ''])
-            else:
-                ips_hostnames.add_row([ip, hostname, OS])
-        except Exception, e:
-            print e
-            continue
-        
+	
+	r = 1
+	for i,ip in enumerate(ip_list):
+		try:
+			nmScan = nmap.PortScanner()
+			results = nmScan.scan(str(ip),arguments='-A -T5')
+			hostname = results['scan'][str(ip)]['hostnames'][0]['name']
+			OS = results['scan'][str(ip)]['osmatch'][0]['name']
+			if OS == None:
+				ips_hostnames.add_row([ip, hostname, ''])
+			else:
+				ips_hostnames.add_row([ip, hostname, OS])
+			a('clear')
+		except Exception, e:
+			print e
+			continue
+		progress_bar.printProgressBar(i + 1, len(ip_list), prefix='Scanning %d of %d...\nProgress:' % (r + 1,len(ip_list)), suffix='Complete', length=50)
+		
 def network_map(ip_list): # pump list of ips into function to map network
 
     G = nx.Graph()
--- progress_bar.py
+++ progress_bar.py
@@ -1,6 +1,6 @@
 # Print iterations progress
 
-def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '>'):
     """
     Call in a loop to create terminal progress bar
     @params:
@@ -15,28 +15,25 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filledLength = int(length * iteration // total)
     bar = fill * filledLength + '-' * (length - filledLength)
-    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+    print('\r%s |%s| %s%% %s\r' % (prefix, bar, percent, suffix))
     # Print New Line on Complete
     if iteration == total: 
-        print()
+        print '\n'
 
 # 
 # Sample Usage
 # 
 
-from time import sleep
+#from time import sleep
 
 # A List of Items
-items = list(range(0, 57))
-l = len(items)
+#items = list(range(0, 57))
+#l = len(items)
 
 # Initial call to print 0% progress
-printProgressBar(0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
-for i, item in enumerate(items):
+#printProgressBar(0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+#for i, item in enumerate(items):
     # Do stuff...
-    sleep(0.1)
+ #   sleep(0.1)
     # Update Progress Bar
-    printProgressBar(i + 1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
-
-# Sample Output
-Progress: |█████████████████████████████████████████████-----| 90.0% Complete
+ #   printProgressBar(i + 1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
